0d819dbe7892f537430877ee804103fdbaf6ced7
JamyCCS <obidullahjamy@gmail.com>
2025-01-05 22:22:25 +0600

    built the GUI interface lmao

---
+++ interface.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter import filedialog, messagebox
+from PIL import Image, ImageTk
+
+
+# GUI interface
+def gui():
+    root = Tk()
+    root.title("Flash Sale Tracker")
+
+  
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    gui()
